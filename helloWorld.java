754509f3976a4d7ce5ff19c554afaa69109a4c66
Menashi30 <menashi30@gmail.com>
2023-01-18 23:13:10 +0530

    Changed the variable a to 10

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 class helloWorld{
     public static void main(String[] args){
-        int a = 1, b = 2;
+        int a = 10, b = 2;
         System.out.println(a+b);
         System.out.println("Hello World from VSCode");
     }
