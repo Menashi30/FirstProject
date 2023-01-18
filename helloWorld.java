62b6b1bf6359561dfb3b068ed197e91ef58ef833
Menashi30 <menashi30@gmail.com>
2023-01-18 22:57:43 +0530

    added 2 int and print hello world

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+class helloWorld{
+    public static void main(String[] args){
+        int a = 1, b = 2;
+        System.out.println(a+b);
+        System.out.println("Hello World from VSCode");
+    }
+}
