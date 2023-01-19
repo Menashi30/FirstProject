c58a5f9b1b5e5664df8ea77db602411ed47e5847
Menashi30 <menashi30@gmail.com>
2023-01-19 13:41:05 +0530

    changed a to 15

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 class helloWorld{
     public static void main(String[] args){
-        int a = 10, b = 2;
+        int a = 15, b = 2;
         System.out.println(a+b);
         System.out.println("Hello World from VSCode");
     }
