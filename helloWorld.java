f1def5fd48a66fb8ce2b9ca874e353ccb8d1cb27
Menashi30 <menashi30@gmail.com>
2023-01-19 23:52:14 +0530

    changed the operator

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,7 @@
 class helloWorld{
     public static void main(String[] args){
         int a = 20, b = 2;
-        System.out.println(a+b);
+        System.out.println(a*b);
         System.out.println("Hello World from VSCode");
     }
 }
